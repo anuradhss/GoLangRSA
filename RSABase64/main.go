24e7f821f0519fcc46de6d1cc561cdcb149d9ff1
Chanaka Caldera <anuradhtuck@gmail.com>
2019-11-28 00:05:19 +0530

    Generating Public Key from base64 string

--- RSABase64/main.go
+++ RSABase64/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"crypto/x509"
+	"encoding/base64"
+	"fmt"
+)
+
+func generatePublickKeyFromBase64(publickeystring string) {
+
+	base64Data := []byte(publickeystring)
+	d := make([]byte, base64.StdEncoding.DecodedLen(len(base64Data)))
+	n, err := base64.StdEncoding.Decode(d, base64Data)
+	if err != nil {
+		fmt.Println("Error Occured @ line 15 :", err)
+		return
+	}
+
+	d = d[:n]
+	key, err := x509.ParsePKCS1PublicKey(d)
+	if err != nil {
+		fmt.Println("Error Occured @ line 22 :", err)
+		return
+	}
+
+	fmt.Println(key)
+}
 
 func main() {
 	fmt.Println("Working Properly")
+	generatePublickKeyFromBase64("MEgCQQCdnQjRuoEXxfQhVIBHEpTzjDfj3Kz/7uaqpax2O73vcz4V0EoF1X/WJcl1Rh+qmxEoOHcGhq4sRwjMqDl6EPQtAgMBAAE=")
 }
