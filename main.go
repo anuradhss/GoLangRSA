a17359983803da7ca3f40bef6e37e28bbe11afce
Chanaka Caldera <anuradhtuck@gmail.com>
2019-11-27 12:22:51 +0530

    Generating RSA standard public and private key pair

--- main.go
+++ main.go
@@ -1,11 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"fmt"
+)
 
+// declaring public and private key to hold keys
+var _publicKey rsa.PublicKey
+var _privateKey rsa.PrivateKey
+
+// generate RSA key pair -> public key and private key
 func generateKeyPair() {
+	reader := rand.Reader
+	bitSize := 512
 
+	key, err := rsa.GenerateKey(reader, bitSize)
+	if err != nil {
+		fmt.Println("Error Encountered : ", err)
+	}
+
+	_publicKey := key.PublicKey
+	_privateKey := key
+
+	fmt.Println("public key : ", _publicKey)
+	fmt.Println("private key : ", _privateKey)
 }
 
+// main function
 func main() {
 	fmt.Println("working fine")
+	generateKeyPair()
 }
