d7431b7a45171f2eedf84edacc6c3001abe364d0
Chanaka Caldera <anuradhtuck@gmail.com>
2019-11-29 12:07:06 +0530

    Adding Decryption function with private key

--- RSAEncryptionDecryption/main.go
+++ RSAEncryptionDecryption/main.go
@@ -30,7 +30,31 @@ func encryptWithPublicKey(message string, publickey *rsa.PublicKey) string {
 	return encryptedString
 }
 
+// decrypt with private key
+func decryptWithPrivateKey(encryptedmessage string, privatekey *rsa.PrivateKey) string {
+
+	byteEncryptedMessage := []byte(encryptedmessage)
+	plainText, err := rsa.DecryptPKCS1v15(rand.Reader, privatekey, byteEncryptedMessage)
+	if err != nil {
+		fmt.Println("Error occured while decryption")
+		return ""
+	}
+
+	decryptedString := string(plainText)
+	return decryptedString
+}
+
 // main function
 func main() {
-	fmt.Println("working fine")
+	fmt.Println("App Starts ...")
+	fmt.Printf("\n\n ========================== Generating RSA Key Pair ========================== \n\n")
+	privatekey, publickey := generateRSAKeypair()
+	fmt.Println("Private Key :", privatekey)
+	fmt.Println("Publick Key : ", publickey)
+	fmt.Printf("\n\n ========================== Enctrypt Given Message With Public Key ========================== \n\n")
+	encryptedMessage := encryptWithPublicKey("hello anuradh", publickey)
+	fmt.Println("Encrypted Message  :", encryptedMessage)
+	fmt.Printf("\n\n ========================== Decrypt Given Encrypted Message With Private Key ========================== \n\n")
+	decryptedMessage := decryptWithPrivateKey(encryptedMessage, privatekey)
+	fmt.Println("Decrypted Message : ", decryptedMessage)
 }
